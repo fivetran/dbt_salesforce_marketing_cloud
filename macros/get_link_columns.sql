db66cb8965517b0342aba8a8f993c4871f57940a
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-12-15 23:30:14 +0100

    update macros

--- macros/get_link_columns.sql
+++ macros/get_link_columns.sql
@@ -1,6 +1,16 @@
 {% macro get_link_columns() %}
 
 {% set columns = [
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
+    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
+    {"name": "id", "datatype": dbt.type_int()},
+    {"name": "alias", "datatype": dbt.type_string()},
+    {"name": "last_clicked", "datatype": dbt.type_timestamp()},
+    {"name": "created_date", "datatype": "datetime"},
+    {"name": "modified_date", "datatype": "datetime"},
+    {"name": "total_clicks", "datatype": dbt.type_int()},
+    {"name": "unique_clicks", "datatype": dbt.type_int()},
+    {"name": "url ", "datatype": dbt.type_string()}
 ] %}
 
 {{ return(columns) }}
--- macros/get_link_send_columns.sql
+++ macros/get_link_send_columns.sql
@@ -1,6 +1,10 @@
 {% macro get_link_send_columns() %}
 
 {% set columns = [
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()},
+    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
+    {"name": "link_id", "datatype": dbt.type_int()},
+    {"name": "send_id", "datatype": dbt.type_int()}
 ] %}
 
 {{ return(columns) }}
