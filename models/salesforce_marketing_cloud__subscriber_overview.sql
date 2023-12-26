cc1ecc13a872347b7a2f3e235e708965caa7a51f
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-12-27 00:06:40 +0100

    updates

--- models/salesforce_marketing_cloud__subscriber_overview.sql
+++ models/salesforce_marketing_cloud__subscriber_overview.sql
@@ -24,7 +24,7 @@ with subscribers as (
         end as days_subscribed, #}
       case when subscribers.unsubscribed_date is not null 
         then {{ dbt.datediff("subscribers.created_date", "subscribers.unsubscribed_date", "day") }} 
-        else {{ dbt.datediff("subscribers.created_date", "current_date()", "day") }} 
+        else {{ dbt.datediff("subscribers.created_date", "current_date", "day") }} 
         end as days_subscribed,
       sum(case when events_enhanced.is_sent then 1 else 0 end) as number_of_sends,
       max(case when events_enhanced.is_sent then events_enhanced.event_date end) as most_recent_send,
