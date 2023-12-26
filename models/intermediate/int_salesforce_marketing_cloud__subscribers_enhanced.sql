af3ec02d2aced0541e9d25405efbe4e0d0680d57
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-12-26 22:09:41 +0100

    updates

--- models/intermediate/int_salesforce_marketing_cloud__subscribers_enhanced.sql
+++ models/intermediate/int_salesforce_marketing_cloud__subscribers_enhanced.sql
@@ -1,5 +1,3 @@
-{{ config(enabled=var('salesforce_marketing_cloud__list_enabled', true)) }}
-
 -- this is meant to be stand alone since subscribers could belong to multiple lists, and I did not want to do a list agg to include this in the subscriber overview, but open for discussion
 with subscribers as ( 
   select 
