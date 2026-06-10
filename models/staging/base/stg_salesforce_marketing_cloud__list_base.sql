71395506b59f6f278962ed84369ac7c0c0055085
Catherine Fritz <111930712+fivetran-catfritz@users.noreply.github.com>
2026-06-10 15:16:31 -0500

    fix base

--- models/staging/base/stg_salesforce_marketing_cloud__list_base.sql
+++ models/staging/base/stg_salesforce_marketing_cloud__list_base.sql
@@ -1,10 +1,12 @@
 {{ config(enabled=var('salesforce_marketing_cloud__list_enabled', true)) }}
 
+{% if var('salesforce_marketing_cloud_union_schemas', []) | length > 0 or var('salesforce_marketing_cloud_union_databases', []) | length > 0 %}
+
 {{
     fivetran_utils.union_data(
-        table_identifier='list', 
-        database_variable='salesforce_marketing_cloud_database', 
-        schema_variable='salesforce_marketing_cloud_schema', 
+        table_identifier='list',
+        database_variable='salesforce_marketing_cloud_database',
+        schema_variable='salesforce_marketing_cloud_schema',
         default_database=target.database,
         default_schema='salesforce_marketing_cloud',
         default_variable='list',
@@ -12,3 +14,15 @@
         union_database_variable='salesforce_marketing_cloud_union_databases'
     )
 }}
+
+{% else %}
+
+{{
+    fivetran_utils.union_connections(
+        connection_dictionary='salesforce_marketing_cloud_sources',
+        single_source_name='salesforce_marketing_cloud',
+        single_table_name='list'
+    )
+}}
+
+{% endif %}
--- models/staging/base/stg_salesforce_marketing_cloud__subscriber_base.sql
+++ models/staging/base/stg_salesforce_marketing_cloud__subscriber_base.sql
@@ -1,8 +1,10 @@
+{% if var('salesforce_marketing_cloud_union_schemas', []) | length > 0 or var('salesforce_marketing_cloud_union_databases', []) | length > 0 %}
+
 {{
     fivetran_utils.union_data(
-        table_identifier='subscriber', 
-        database_variable='salesforce_marketing_cloud_database', 
-        schema_variable='salesforce_marketing_cloud_schema', 
+        table_identifier='subscriber',
+        database_variable='salesforce_marketing_cloud_database',
+        schema_variable='salesforce_marketing_cloud_schema',
         default_database=target.database,
         default_schema='salesforce_marketing_cloud',
         default_variable='subscriber',
@@ -10,3 +12,15 @@
         union_database_variable='salesforce_marketing_cloud_union_databases'
     )
 }}
+
+{% else %}
+
+{{
+    fivetran_utils.union_connections(
+        connection_dictionary='salesforce_marketing_cloud_sources',
+        single_source_name='salesforce_marketing_cloud',
+        single_table_name='subscriber'
+    )
+}}
+
+{% endif %}
